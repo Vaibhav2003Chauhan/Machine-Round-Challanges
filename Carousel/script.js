575d054ffc62c5f8566abe7c5881bc6d62d52d77
Vaibhav Chauhan <vishuchauhan461@gmail.com>
2024-12-07 23:30:43 +0530

    added the code and changes accordingly completed!!!!

--- Carousel/script.js
+++ Carousel/script.js
@@ -6,25 +6,20 @@ imagelist = ['https://images.unsplash.com/photo-1732948937655-095f68551734?w=500
     'https://images.unsplash.com/photo-1732979888718-47a5c6e454d9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzNXx8fGVufDB8fHx8fA%3D%3D'
 ]
 
-let currentIndex = 0; // Track the current image index
-const carouselImageContainer = document.querySelector('.carousel-img');
+let currentIndex = 0;
+let carouselImageContainer = document.querySelector('.carousel-img')
 
-// Function to display the current image
 function displayImage() {
-    carouselImageContainer.innerHTML = `<img src="${imagelist[currentIndex]}" alt="Image ${currentIndex + 1}">`;
+    carouselImageContainer.innerHTML = `<img src="${imagelist[currentIndex]}" alt="Image ${currentIndex + 1}">
+    <h2>Image ${currentIndex+1}</h2>`
 }
 
-// Move to the previous image
 function prev_image() {
-    currentIndex = (currentIndex - 1 + imagelist.length) % imagelist.length; // Wrap around to the last image
+    currentIndex = (currentIndex - 1 + imagelist.length) % imagelist.length
     displayImage();
 }
-
-// Move to the next image
 function next_image() {
-    currentIndex = (currentIndex + 1) % imagelist.length; // Wrap around to the first image
+    currentIndex = (currentIndex + 1) % imagelist.length
     displayImage();
 }
-
-// Initialize the carousel
 displayImage();
